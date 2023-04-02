2ac9e99bf90496619b1bc8eb5b5e45eab8c99d49
МАРК <markpan07@mail.ru>
2023-04-02 13:20:00 +0300

    Initial commit

---
+++ src/main/java/pro/sky/collections1/Collections1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.collections1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Collections1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Collections1Application.class, args);
+    }
+
+}
